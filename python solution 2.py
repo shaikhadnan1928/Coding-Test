90852498403959ef9fd2a726cf5d7bedfccb9539
shaikhadnan1928 <artsmarty05@gmail.com>
2024-08-02 17:07:33 +0530

    Add files via upload

---
+++ python solution 2.py
@@ -0,0 +1,14 @@
+def reverse_string(s):
+    return s[::-1]
+
+# Example usage
+input_string = "Hello, World!"
+reversed_string = reverse_string(input_string)
+print(f"Original string: {input_string}")
+print(f"Reversed string: {reversed_string}")
+
+     
+"""
+Original string: Hello, World!
+Reversed string: !dlroW ,olleH
+"""
---
+++ python solution 3.py
@@ -0,0 +1,14 @@
+def count_occurrences(lst, element):
+    return lst.count(element)
+
+# Example usage
+input_list = [1, 2, 3, 4, 2, 2, 5, 2, 6, 2]
+element_to_count = 2
+occurrences = count_occurrences(input_list, element_to_count)
+print(f"Element {element_to_count} occurs {occurrences} times in the list.")
+
+
+"""
+Element 2 occurs 5 times in the list.
+"""
+     
